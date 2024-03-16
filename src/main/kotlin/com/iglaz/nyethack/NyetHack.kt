2bf13c2db616804f411850fcaca4ed300a798fa0
IlyaGlaz <glazterix@gmail.com>
2024-03-16 20:52:04 +0300

    функция изменения настроения рассказчика

--- src/main/kotlin/com/iglaz/nyethack/NyetHack.kt
+++ src/main/kotlin/com/iglaz/nyethack/NyetHack.kt
@@ -3,4 +3,7 @@ package com.iglaz.nyethack
 fun main() {
     narrate("A hero enters the town of Kronstadt. What is their name?")
     val heroName = readLine() ?: ""
+
+    changeNarratorMood()
+    narrate("$heroName heads to the town square")
 }
